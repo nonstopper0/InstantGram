651df5d7f060301c694fff10f91b12f44b15bc14
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2019-12-20 08:58:16 -0700

    fixed small server.js issue

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const methodOverride = require('method-override');
-const app = express.Router();
+const app = express();
 const port = 3000
 require('./db/db.js');
 
 app.use(express.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
-
-
 
 app.listen(port, () => {
     console.log('Listening on port ' + port)
